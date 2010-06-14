b4ade8352de16bcd29957d6ba63d4b01800f00f4
Stef De Pooter <stef@borgraf.cs.kuleuven.be>
2010-06-14 15:39:44 +0200

    added stuff for output type option

--- options.h
+++ options.h
@@ -8,22 +8,25 @@
 #define OPTIONS_H
 
 enum WarningTypes { WT_FREE_VARS=0, WT_VARORCONST=1, WT_SORTDERIVE=2, WT_STDIN=3 };
+enum OutputType { OT_TEXT, OT_IDP };
 
 struct Options {
 
-		  // Attributes
-		  bool				_statistics;		// print statistics on stderr iff _statistics=true
-		  bool				_verbose;			// print extra information on stderr iff _verbose=true
-		  bool				_readfromstdin;		// expect input from stdin iff _readfromstdin=true
-		  vector<bool>		_warning;			// _warning[n] = true means that warnings of type n are not suppressed
+		// Attributes
+		bool			_statistics;		// print statistics on stderr iff _statistics=true
+		bool			_verbose;			// print extra information on stderr iff _verbose=true
+		bool			_readfromstdin;		// expect input from stdin iff _readfromstdin=true
+		vector<bool>	_warning;			// _warning[n] = true means that warnings of type n are not suppressed
+		OutputType		_outputtype;
 
-		  // Constructor
-		  Options() {
+		// Constructor
+		Options() {
 				_statistics = false;
 				_verbose = false;
 				_readfromstdin = false;
 				_warning = vector<bool>(4,true);
-		  }
+				_outputtype = OT_TEXT;
+		}
 
 };
 
