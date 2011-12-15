0a17e23521b5107c8329fd0fbf3c69ed074de548
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2011-12-15 14:17:58 +0100

    fixes issue 61: added copyright

--- commands/pushnegations.hpp
+++ commands/pushnegations.hpp
@@ -1,8 +1,12 @@
-/************************************
-  	pushnegations.hpp
-	this file belongs to GidL 2.0
-	(c) K.U.Leuven
-************************************/
+/****************************************************************
+* Copyright 2010-2012 Katholieke Universiteit Leuven
+*  
+* Use of this software is governed by the GNU LGPLv3.0 license
+* 
+* Written by Broes De Cat, Stef De Pooter, Johan Wittockx
+* and Bart Bogaerts, K.U.Leuven, Departement Computerwetenschappen,
+* Celestijnenlaan 200A, B-3001 Leuven, Belgium
+****************************************************************/
 
 #ifndef CMD_PUSHNEGATIONS_HPP_
 #define CMD_PUSHNEGATIONS_HPP_
--- theoryinformation/CheckContainment.hpp
+++ theoryinformation/CheckContainment.hpp
@@ -1,3 +1,13 @@
+/****************************************************************
+* Copyright 2010-2012 Katholieke Universiteit Leuven
+*  
+* Use of this software is governed by the GNU LGPLv3.0 license
+* 
+* Written by Broes De Cat, Stef De Pooter, Johan Wittockx
+* and Bart Bogaerts, K.U.Leuven, Departement Computerwetenschappen,
+* Celestijnenlaan 200A, B-3001 Leuven, Belgium
+****************************************************************/
+
 #ifndef CONTAINMENTCHECKER_HPP_
 #define CONTAINMENTCHECKER_HPP_
 
