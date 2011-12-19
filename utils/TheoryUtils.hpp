d1b9912feab7191eda8e08da5645c6cc8d113ad1
Stef De Pooter <stef.depooter@cs.kuleuven.be>
2011-12-19 15:42:59 +0100

    Removed unused old files...
    ... for graphing functions and aggregates, these files were combined into GraphFuncsAndAggs.*pp.

--- utils/TheoryUtils.hpp
+++ utils/TheoryUtils.hpp
@@ -62,22 +62,22 @@ void transform(Construct* object, Values ... parameters) {
 }
 
 namespace FormulaUtils {
-/** Returns true iff the aggregate formula is anti-monotone **/
+/** Returns true iff the aggregate formula is anti-monotone */
 bool isAntimonotone(const AggForm* af);
 
-/** Returns true iff the aggregate formula is monotone **/
+/** Returns true iff the aggregate formula is monotone */
 bool isMonotone(const AggForm* af);
 
-/** Create the formula 'true' **/
+/** Create the formula 'true' */
 BoolForm* trueFormula();
 
-/** Create the formula 'false' **/
+/** Create the formula 'false' */
 BoolForm* falseFormula();
 
-/** Check sorts in the given formula **/
+/** Check sorts in the given formula */
 void checkSorts(Vocabulary* v, Formula* f);
 
-/** Returns true iff at least one FuncTerm/AggTerm occurs in the given formula **/
+/** Returns true iff at least one FuncTerm/AggTerm occurs in the given formula */
 bool containsFuncTerms(Formula* f);
 bool containsAggTerms(Formula* f);
 bool containsSymbol(const PFSymbol* s, const Formula* f);
@@ -92,117 +92,118 @@ void deriveSorts(Vocabulary* v, Formula* f);
  */
 double estimatedCostAll(PredForm* query, const std::set<Variable*> freevars, bool inverse, AbstractStructure* structure);
 
-/** Flatten all nested formulas **/
+/** Flatten all nested formulas */
 Formula* flatten(Formula*);
 
-/** Recursively rewrite all function terms to their predicate form **/
+/** Recursively rewrite all function terms to their predicate form, and aggregate terms to aggregate formulas */
 Formula* graphFuncsAndAggs(Formula* f, AbstractStructure* str = NULL, Context con = Context::POSITIVE);
 
-/** Push negations inside **/
+/** Push negations inside */
 Formula* pushNegations(Formula* f);
 
-/** Rewrite all equivalences into implications **/
+/** Rewrite all equivalences into implications */
 Formula* removeEquivalences(Formula*);
 
-/** Recursively rewrite all EqChainForms in the given formula to BoolForms **/
+/** Recursively rewrite all EqChainForms in the given formula to BoolForms */
 Formula* splitComparisonChains(Formula*, Vocabulary* voc = NULL);
 
 Formula* splitIntoMonotoneAgg(Formula* f);
 
-/** Split product aggregates in aggregates conaining only positive values. **/
+/** Split product aggregates in aggregates conaining only positive values. */
 Formula* splitProducts(Formula* f);
 
-/** Replace the given term by the given variable in the given formula **/
+/** Replace the given term by the given variable in the given formula */
 Formula* substituteTerm(Formula*, Term*, Variable*);
 
-/** Recursively move all function and aggregate terms **/
+/** Recursively move all function and aggregate terms */
 Formula* unnestFuncsAndAggs(Formula*, AbstractStructure* str = NULL, Context con = Context::POSITIVE);
 
-/** Recursively move all partial terms outside atoms **/
+/** Recursively move all partial terms outside atoms */
 Formula* unnestPartialTerms(Formula*, Context con = Context::POSITIVE, 
 		AbstractStructure* str = NULL, Vocabulary* voc = NULL);
 
-/** Recursively remove all nested terms **/
+/** Recursively remove all nested terms */
 Formula* unnestTerms(Formula*, Context con = Context::POSITIVE, AbstractStructure* str = NULL, 
 		Vocabulary* voc = NULL);
 
-/** Non-recursively move terms that are three-valued in a given structure outside of the given atom **/
+/** Non-recursively move terms that are three-valued in a given structure outside of the given atom */
 Formula* unnestThreeValuedTerms(Formula*, AbstractStructure*, Context context, 
 		bool cpsupport = false, const std::set<const PFSymbol*> cpsymbols = std::set<const PFSymbol*>());
 
 
 
-/** Replace all definitions in the theory by their completion **/
+/** Replace all definitions in the theory by their completion */
 AbstractTheory* addCompletion(AbstractTheory*);
 
-/** Rewrite (! x : ! y : phi) to (! x y : phi), rewrite ((A & B) & C) to (A & B & C), etc. **/
+/** Rewrite (! x : ! y : phi) to (! x y : phi), rewrite ((A & B) & C) to (A & B & C), etc. */
 AbstractTheory* flatten(AbstractTheory*);
 
 /** Rewrite (F(x) = y) or (y = F(x)) to Graph_F(x,y) 
-  * Rewrite (AggTerm op BoundTerm) to an aggregate formula (op = '=', '<', or '>') **/
+  * Rewrite (AggTerm op BoundTerm) to an aggregate formula (op = '=', '<', or '>') */
 AbstractTheory* graphFuncsAndAggs(AbstractTheory*, AbstractStructure* str = NULL, Context con = Context::POSITIVE);
 
-/** Merge two theories **/
+/** Merge two theories */
 AbstractTheory* merge(AbstractTheory*, AbstractTheory*);
 
-/** Count the number of subformulas in the theory **/
+/** Count the number of subformulas in the theory */
 int nrSubformulas(AbstractTheory*);
 
-/** Push negations inside **/
+/** Push negations inside */
 AbstractTheory* pushNegations(AbstractTheory*);
 
-/** Rewrite (! x : phi & chi) to ((! x : phi) & (!x : chi)), and similarly for ?. **/
+/** Rewrite (! x : phi & chi) to ((! x : phi) & (!x : chi)), and similarly for ?. */
 AbstractTheory* pushQuantifiers(AbstractTheory*);
 
-/** Rewrite A <=> B to (A => B) & (B => A) **/
+/** Rewrite A <=> B to (A => B) & (B => A) */
 AbstractTheory* removeEquivalences(AbstractTheory*);
 
-/** Rewrite chains of equalities to a conjunction or disjunction of atoms. **/
+/** Rewrite chains of equalities to a conjunction or disjunction of atoms. */
 AbstractTheory* splitComparisonChains(AbstractTheory*, Vocabulary* voc = NULL);
 
-/** Split product aggregates in aggregates conaining only positive values. **/
+/** Split product aggregates in aggregates conaining only positive values. */
 AbstractTheory* splitProducts(AbstractTheory* f);
 
-/** Recursively move all function and aggregate terms **/
+/** Recursively move all function and aggregate terms */
 AbstractTheory* unnestFuncsAndAggs(AbstractTheory*, AbstractStructure* str = NULL,
 		Context con = Context::POSITIVE);
 
-/** Rewrite the theory so that there are no nested terms **/
+/** Rewrite the theory so that there are no nested terms */
 AbstractTheory* unnestTerms(AbstractTheory*, Context con = Context::POSITIVE, 
 		AbstractStructure* str = NULL, Vocabulary* voc = NULL);
 }
 
 namespace TermUtils {
-/** Check sorts in the given term **/
+/** Check sorts in the given term */
 void checkSorts(Vocabulary*, Term*);
 
-/** Derive sorts in the given term **/
+/** Derive sorts in the given term */
 void deriveSorts(Vocabulary*, Term*);
 
+/** Check whether the term is partial (contains partial functions) */
 bool isPartial(Term*);
 }
 
 namespace SetUtils {
 /** Returns false if the set expression is not two-valued in the given structure. 
- May return true if the set expression is two-valued in the structure. **/
+ May return true if the set expression is two-valued in the structure. */
 bool approxTwoValued(SetExpr*, AbstractStructure*);
 
-/** Rewrite set expressions by moving three-valued terms **/
+/** Rewrite set expressions by moving three-valued terms */
 SetExpr* unnestThreeValuedTerms(SetExpr*, AbstractStructure*, Context context, bool cpsupport = false, 
 		const std::set<const PFSymbol*> cpsymbols = std::set<const PFSymbol*>());
 }
 
 namespace DefinitionUtils {
-/** Check sorts in the given rule **/
+/** Check sorts in the given rule */
 void checkSorts(Vocabulary* v, Rule* f);
 
-/** Derive sorts in the given rule **/
+/** Derive sorts in the given rule */
 void deriveSorts(Vocabulary* v, Rule* f);
 
-/** Compute the open symbols of a definition **/
+/** Compute the open symbols of a definition */
 std::set<PFSymbol*> opens(Definition*);
 
-/** Non-recursively move terms that are three-valued in a given structure outside of the head of the rule **/
+/** Non-recursively move terms that are three-valued in a given structure outside of the head of the rule */
 Rule* unnestThreeValuedTerms(Rule*, AbstractStructure*, Context context, bool cpsupport = false, 
 		const std::set<const PFSymbol*> cpsymbols = std::set<const PFSymbol*>());
 }
