3f6005a9c6c785abf099484f07c48e8c912f646c
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2012-01-09 16:41:03 +0100

    fixed the issue with the alltwovaluedstructures

--- src/structure.cpp
+++ src/structure.cpp
@@ -4199,6 +4199,7 @@ void Structure::makeTwoValued() {
 						continue;
 					}
 					inter->graphInter()->makeTrue(tuple);
+					break;
 				}
 			}
 		} else {
@@ -4216,7 +4217,6 @@ void Structure::makeTwoValued() {
 			throw IdpException("Terminate requested");
 		}
 	}
-
 	//If some predicate is not two-valued, calculate all structures that are more precise in which this function is two-valued
 	for (auto i = _predinter.begin(); i != _predinter.end(); i++) {
 		auto inter = (*i).second;
@@ -4450,6 +4450,13 @@ void completeSortTable(const PredTable* pt, PFSymbol* symbol, const string& stru
 
 void addUNAPattern(Function*) {
 	throw notyetimplemented("una pattern type");
+}
+
+void AbstractStructure::put(std::ostream& s){
+	IDPPrinter<std::ostream> p = IDPPrinter<std::ostream>(s);
+	p.startTheory();
+	p.visit(this);
+	p.endTheory();
 }
 
 void Structure::autocomplete() {
