bf69f0ced085ae0fe6dc21750a7d2c20fc1e4b7d
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2012-01-11 16:22:58 +0100

    removed builds directory

--- lib/gtestparser/builds/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/data/documenten/kuldoctoraat/software/gtestparser
-# Build directory: /media/data/documenten/kuldoctoraat/software/gtestparser/builds
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
