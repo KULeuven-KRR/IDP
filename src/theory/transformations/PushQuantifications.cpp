7614f43ebaba1d34cf179aaa827d0a26eb0a9b8c
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-03-06 13:27:30 +0100

    Bugfix in pushing quantifications inside.

--- src/theory/transformations/PushQuantifications.cpp
+++ src/theory/transformations/PushQuantifications.cpp
@@ -13,40 +13,6 @@
 
 using namespace std;
 
-/*std::set<Variable*> clone(const std::set<Variable*>& origvars) {
-	std::set<Variable*> newvars;
-	for (auto it = origvars.cbegin(); it != origvars.cend(); ++it) {
-		auto v = new Variable((*it)->name(), (*it)->sort(), (*it)->pi());
-		newvars.insert(v);
-	}
-	return newvars;
-}*/
-
-// old code, absolutely not useful
-/*Formula* PushQuantifications::visit(QuantForm* qf) {
-	if (not sametypeid<BoolForm>(*(qf->subformula()))) {
-		return TheoryMutatingVisitor::visit(qf);
-	}
-
-	auto bf = dynamic_cast<BoolForm*>(qf->subformula());
-	auto u = qf->isUnivWithSign() ? QUANT::UNIV : QUANT::EXIST;
-	auto c = bf->isConjWithSign();
-
-	if ((u == QUANT::UNIV && not bf->isConjWithSign()) || (u == QUANT::EXIST && bf->isConjWithSign())) {
-		return TheoryMutatingVisitor::visit(qf);
-	}
-
-	auto s = (qf->sign() == bf->sign()) ? SIGN::POS : SIGN::NEG;
-	vector<Formula*> vf;
-	for (auto it = bf->subformulas().cbegin(); it != bf->subformulas().cend(); ++it) {
-		vf.push_back(new QuantForm(s, u, clone(qf->quantVars()), (*it)->clone(), FormulaParseInfo()));
-	}
-	auto nbf = new BoolForm(SIGN::POS, c, vf, (qf->pi()));
-	qf->subformula()->recursiveDelete();
-	delete (qf);
-	return nbf->accept(this);
-}*/
-
 template<class VarList>
 bool sharesVars(const VarList& listone, const VarList& listtwo){
 	for(auto i=listone.cbegin(); i!=listone.cend(); ++i) {
@@ -79,47 +45,48 @@ Formula* PushQuantifications::visit(QuantForm* qf) {
 	bool conj = true;
 	vector<QuantForm*> quantforms;
 	quantforms.push_back(qf);
+	auto currentquant = qf;
 
 	while(true){
-		auto qsubf = dynamic_cast<QuantForm*>(qf->subformula());
+		auto qsubf = dynamic_cast<QuantForm*>(currentquant->subformula());
 		if(qsubf!=NULL){
 			quantforms.push_back(qsubf);
-			qf = qsubf;
+			currentquant = qsubf;
 		}else{
-			auto bsubf = dynamic_cast<BoolForm*>(qf->subformula());
-			if(bsubf!=NULL){
-				vector<vector<Formula*> > formulalevels(quantforms.size()+1); // Level at which they should be added (index 0 is before the first quantifier, ...)
-				conj = bsubf->conj();
-				Assert(bsubf->sign()==SIGN::POS);
-				for(auto i=bsubf->subformulas().cbegin(); i<bsubf->subformulas().cend(); ++i) {
-					auto subvars = (*i)->freeVars();
-					int index = quantforms.size();
-					while(index>0 && not sharesVars(subvars, quantforms[index-1]->quantVars())){
-						index--;
-					}
-					formulalevels[index].push_back(*i);
-				}
-				// Construct new formula:
-				Formula* prevform = NULL;
-				for(int i=quantforms.size()-1; i>-1; --i){
-					vector<vector<Formula*> > splitformulalevels;
-					vector<Variable*> splitvariables;
-					splitOverSameQuant(quantforms[i]->quantVars(), formulalevels[i+1], splitformulalevels, splitvariables);
-					for(int j=splitvariables.size()-1; j>-1; --j){
-						auto subformulas = splitformulalevels[j];
-						if(prevform!=NULL){
-							subformulas.push_back(prevform);
-						}
-						prevform = new QuantForm(SIGN::POS, quantforms[i]->quant(), {splitvariables[j]}, new BoolForm(SIGN::POS, conj, subformulas, bsubf->pi()), quantforms[i]->pi());
-					}
-				}
-				auto subformulas = formulalevels[0];
-				Assert(prevform!=NULL);
-				subformulas.push_back(prevform);
-				return new BoolForm(SIGN::POS, conj, subformulas, bsubf->pi());
-			}else{
+			auto bsubf = dynamic_cast<BoolForm*>(currentquant->subformula());
+			if(bsubf==NULL){
 				return qf;
 			}
+			vector<vector<Formula*> > formulalevels(quantforms.size()+1); // Level at which they should be added (index 0 is before the first quantifier, ...)
+			conj = bsubf->conj();
+			Assert(bsubf->sign()==SIGN::POS);
+			for(auto i=bsubf->subformulas().cbegin(); i<bsubf->subformulas().cend(); ++i) {
+				auto subvars = (*i)->freeVars();
+				int index = quantforms.size();
+				while(index>0 && not sharesVars(subvars, quantforms[index-1]->quantVars())){
+					index--;
+				}
+				formulalevels[index].push_back(*i);
+			}
+			// Construct new formula:
+			Formula* prevform = NULL;
+			for(int i=quantforms.size()-1; i>-1; --i){
+				vector<vector<Formula*> > splitformulalevels;
+				vector<Variable*> splitvariables;
+				splitOverSameQuant(quantforms[i]->quantVars(), formulalevels[i+1], splitformulalevels, splitvariables);
+				Assert(splitvariables.size()>0);
+				for(int j=splitvariables.size()-1; j>-1; --j){
+					auto subformulas = splitformulalevels[j];
+					if(prevform!=NULL){
+						subformulas.push_back(prevform);
+					}
+					prevform = new QuantForm(SIGN::POS, quantforms[i]->quant(), {splitvariables[j]}, new BoolForm(SIGN::POS, conj, subformulas, bsubf->pi()), quantforms[i]->pi());
+				}
+			}
+			auto subformulas = formulalevels[0];
+			Assert(prevform!=NULL);
+			subformulas.push_back(prevform);
+			return new BoolForm(SIGN::POS, conj, subformulas, bsubf->pi());
 		}
 	}
 }
