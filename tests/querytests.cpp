4a10bf5a31cd69e6f037dbcef608fbc4d5fe5516
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2012-03-09 12:15:45 +0100

    added query tests

---
+++ tests/querytests.cpp
@@ -0,0 +1,40 @@
+/****************************************************************
+* Copyright 2010-2012 Katholieke Universiteit Leuven
+*  
+* Use of this software is governed by the GNU LGPLv3.0 license
+* 
+* Written by Broes De Cat, Stef De Pooter, Johan Wittocx
+* and Bart Bogaerts, K.U.Leuven, Departement Computerwetenschappen,
+* Celestijnenlaan 200A, B-3001 Leuven, Belgium
+****************************************************************/
+
+#include <cmath>
+
+#include "gtest/gtest.h"
+#include "external/rungidl.hpp"
+#include "utils/FileManagement.hpp"
+#include "TestUtils.hpp"
+
+#include <exception>
+
+using namespace std;
+
+namespace Tests {
+
+vector<string> generateListOfQueryFiles() {
+	vector<string> testdirs {"simple/"};
+	return getAllFilesInDirs(getTestDirectory() + "query/", testdirs);
+}
+
+class QueryTest: public ::testing::TestWithParam<string> {
+};
+
+TEST_P(QueryTest, DoesQuerying) {
+	runTests("querytest.idp", GetParam());
+}
+
+
+INSTANTIATE_TEST_CASE_P(ModelExpansion, QueryTest, ::testing::ValuesIn(generateListOfQueryFiles()));
+
+
+}
