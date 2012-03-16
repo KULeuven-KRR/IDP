6370cefce2d9e296e79d9a261a362c5d137e63bb
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-03-16 15:13:02 +0100

    Added command which parses a file in lua and added test for it.

---
+++ src/external/commands/parse.hpp
@@ -0,0 +1,32 @@
+/****************************************************************
+ * Copyright 2010-2012 Katholieke Universiteit Leuven
+ *  
+ * Use of this software is governed by the GNU LGPLv3.0 license
+ * 
+ * Written by Broes De Cat, Stef De Pooter, Johan Wittocx
+ * and Bart Bogaerts, K.U.Leuven, Departement Computerwetenschappen,
+ * Celestijnenlaan 200A, B-3001 Leuven, Belgium
+ ****************************************************************/
+
+#ifndef PARSE_HPP_
+#define PARSE_HPP_
+
+#include "commandinterface.hpp"
+#include "IncludeComponents.hpp"
+
+extern void parsefile(const std::string&);
+
+class ParseInference: public StringBase {
+public:
+	ParseInference()
+			: StringBase("parse",
+					"Parses the given file and adds its information into the datastructures.") {
+	}
+
+	InternalArgument execute(const std::vector<InternalArgument>& args) const {
+		parsefile(*get<0>(args));
+		return nilarg();
+	}
+};
+
+#endif /* PARSE_HPP_ */
