8b5f0a9b1808b3a8df7acd9866d3c209f1c6d9eb
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-03-18 16:13:18 +0100

    Removed random polarity choice for lazy atoms, it should be only the initial polarity choice which is random.

--- src/inferences/SolverConnection.cpp
+++ src/inferences/SolverConnection.cpp
@@ -27,7 +27,7 @@ MinisatID::WrappedPCSolver* createsolver(int nbmodels) {
 	modes.verbosity = options->getValue(IntType::SATVERBOSITY);
 
 	modes.polarity = MinisatID::POL_STORED;
-	if(getOption(BoolType::MXRANDOMPOLARITYCHOICE) || getOption(BoolType::GROUNDLAZILY)){
+	if(getOption(BoolType::MXRANDOMPOLARITYCHOICE)){
 		modes.polarity = MinisatID::POL_RAND;
 	}
 
