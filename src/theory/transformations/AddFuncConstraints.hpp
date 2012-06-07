b4bb90a0a1601d69861413701c1372e1a38adb84
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-06-07 14:08:32 +0200

    Corrected addition of function constraints. Fixed test.

--- src/theory/transformations/AddFuncConstraints.hpp
+++ src/theory/transformations/AddFuncConstraints.hpp
@@ -20,20 +20,21 @@
 class Function;
 class Vocabulary;
 
-class AddFuncConstraints : public DefaultTraversingTheoryVisitor{
+class AddFuncConstraints: public DefaultTraversingTheoryVisitor {
 private:
 	std::set<Function*> _functions;
 	bool _cpsupport;
 	Vocabulary* _voc;
 public:
-	void execute(AbstractTheory* theory, std::map<Function*, Formula*>& funcconstraints, bool cpsupport) {
-		Assert(theory!=NULL);
+	template<class T>
+	void execute(T obj, Vocabulary* voc, std::map<Function*, Formula*>& funcconstraints, bool cpsupport) {
+		Assert(obj!=NULL);
 
-		_voc = theory->vocabulary();
+		_voc = voc;
 		_cpsupport = cpsupport;
 		_functions.clear();
 
-		theory->accept(this);
+		obj->accept(this);
 
 		for (auto i = _functions.cbegin(); i != _functions.cend(); ++i) {
 			if (funcconstraints.find(*i) == funcconstraints.cend() && not (*i)->builtin()) {
@@ -43,51 +44,55 @@ public:
 	}
 
 private:
-	void visit(const PredForm* pf){
+	void visit(const PredForm* pf) {
 		traverse(pf);
-		if(isa<Function>(*pf->symbol())){
+		if (isa<Function>(*pf->symbol())) {
 			_functions.insert(dynamic_cast<Function*>(pf->symbol()));
 		}
 	}
-	void visit(const FuncTerm* f){
+	void visit(const FuncTerm* f) {
+		traverse(f);
 		if (not _cpsupport || not CPSupport::eligibleForCP(f->function(), _voc)) {
 			_functions.insert(f->function());
 		}
 	}
 
 	Formula* createFuncConstraints(Function* function) {
-	//Atom: F(x)=y
-	auto vars = VarUtils::makeNewVariables(function->sorts());
-	auto terms = TermUtils::makeNewVarTerms(vars);
-	auto atom = new PredForm(SIGN::POS, function, terms, FormulaParseInfo());
-	auto y = vars.back();
-	std::set<Variable*> yset = { y };
-	vars.pop_back();
-	std::set<Variable*> xset(vars.cbegin(), vars.cend());
+		if (verbosity() > 1) {
+			cerr << "Adding function constraint for " << toString(function) << "\n";
+		}
+		//Atom: F(x)=y
+		auto vars = VarUtils::makeNewVariables(function->sorts());
+		auto terms = TermUtils::makeNewVarTerms(vars);
+		auto atom = new PredForm(SIGN::POS, function, terms, FormulaParseInfo());
+		auto y = vars.back();
+		std::set<Variable*> yset = { y };
+		vars.pop_back();
+		std::set<Variable*> xset(vars.cbegin(), vars.cend());
 
-	//{y|F(x)=y}
-	auto natsort = get(STDSORT::NATSORT);
-	auto one = createDomElem(1);
-	auto oneterm = new DomainTerm(natsort, one, TermParseInfo());
-	auto set = new QuantSetExpr(yset, atom, oneterm, SetParseInfo());
+		//{y|F(x)=y}
+		auto natsort = get(STDSORT::NATSORT);
+		auto one = createDomElem(1);
+		auto oneterm = new DomainTerm(natsort, one, TermParseInfo());
+		auto set = new QuantSetExpr(yset, atom, oneterm, SetParseInfo());
 
-	auto comp = CompType::EQ;
-	if (function->partial()) {
-		comp = CompType::GEQ;
+		auto comp = CompType::EQ;
+		if (function->partial()) {
+			comp = CompType::GEQ;
+		}
+
+		//#{y|F(x) = y} (= or =<) 1
+		auto aggform = new AggForm(SIGN::POS, oneterm->clone(), comp, new AggTerm(new EnumSetExpr( { set }, set->pi()), AggFunction::CARD, TermParseInfo()),
+				FormulaParseInfo()); //double usage of oneterm ===> clone!
+		Formula* final;
+		if (function->sorts().size() == 1) {
+			final = aggform;
+		} else {
+			//!x: #{y|F(x) = y} (= or =<) 1
+			final = new QuantForm(SIGN::POS, QUANT::UNIV, xset, aggform, FormulaParseInfo());
+		}
+		return final;
 	}
-
-	//#{y|F(x) = y} (= or =<) 1
-	auto aggform = new AggForm(SIGN::POS, oneterm->clone(), comp, new AggTerm(new EnumSetExpr( { set }, set->pi()), AggFunction::CARD, TermParseInfo()),
-			FormulaParseInfo()); //double usage of oneterm ===> clone!
-	Formula* final;
-	if (function->sorts().size() == 1) {
-		final = aggform;
-	} else {
-		//!x: #{y|F(x) = y} (= or =<) 1
-		final = new QuantForm(SIGN::POS, QUANT::UNIV, xset, aggform, FormulaParseInfo());
-	}
-	return final;
-}
 };
 
 #endif /* ADDFUNCCON_HPP_ */
