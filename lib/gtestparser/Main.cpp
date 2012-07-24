78c44a98353a5f08c0d2c7d835c1323e3d6e2816
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-07-24 20:37:28 +0200

    Updated gtestparser to correctly detect segmentation faults and correctly parse the remaining file.

--- lib/gtestparser/Main.cpp
+++ lib/gtestparser/Main.cpp
@@ -8,14 +8,21 @@ extern FILE* yyin;
 extern int yyparse();
 extern std::stringstream results;
 extern std::stringstream ss;
+extern std::stringstream error;
 
 using namespace std;
 
 int main(int, char**) {
 	yyin = stdin;
 	auto result = yyparse();
-	if (ss.str() != "") {
-		cerr << "ERROR ENCOUNTERED, ABORTED EARLY: \n" << ss.str() << "\n";
+	if (ss.str() != "" || error.str()!="") {
+		cerr << "ERROR ENCOUNTERED, ABORTED EARLY (possibly segfault): \n";
+		if(error.str()!=""){
+			cerr << error.str() << "\n";
+		}
+		if(ss.str()!=""){
+			cerr << ss.str() << "\n";
+		}
 	} else {
 		cout << "[   SUMMARY   ]\n";
 		cout << results.str();
