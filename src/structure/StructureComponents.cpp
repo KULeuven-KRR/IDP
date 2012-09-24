KULeuven-KRR/IDP

commit 91588a8be3c2f1dbc72eaa4596c298d4fdabda2f

Fixed range handling in internal tables. Note: currently only for integer, can be extended in future.

src/structure/StructureComponents.cpp
@@ -145,16 +145,16 @@ const Universe& FuncTable::universe() const {
 	return _universe;
 }
 
-SortTable* TableUtils::createSortTable(){
+SortTable* TableUtils::createSortTable() {
 	return new SortTable(new EnumeratedInternalSortTable());
 }
-SortTable* TableUtils::createSortTable(int start, int end){
+SortTable* TableUtils::createSortTable(int start, int end) {
 	return new SortTable(new IntRangeInternalSortTable(min(start, end), max(start, end)));
 }
-PredTable* TableUtils::createPredTable(const Universe& universe){
+PredTable* TableUtils::createPredTable(const Universe& universe) {
 	return new PredTable(new EnumeratedInternalPredTable(), universe);
 }
-PredTable* TableUtils::createFullPredTable(const Universe& universe){
+PredTable* TableUtils::createFullPredTable(const Universe& universe) {
 	return new PredTable(new FullInternalPredTable(), universe);
 }
 
@@ -1783,19 +1783,20 @@ bool EnumeratedInternalSortTable::contains(const DomainElement* d) const {
 }
 
 bool EnumeratedInternalSortTable::isRange() const {
-	if (_table.empty()) {
+	if (_table.empty() || nbNotIntElements > 0) {
 		return false;
 	}
 	auto f = first();
 	auto l = last();
-	if (f->type() == DET_INT) {
-		return l->value()._int - f->value()._int == (int) _table.size() - 1;
-	} else {
-		return false;
-	}
+	Assert(f->type() == DET_INT && l->type() == DET_INT);
+	// (and in fact all in between)
+	return l->value()._int - f->value()._int == (int) _table.size() - 1;
 }
 
 void EnumeratedInternalSortTable::addNonRange(const DomainElement* elem, int start, int end) {
+	if (elem->type() != DomainElementType::DET_INT) {
+		nbNotIntElements++;
+	}
 	_table.insert(elem);
 	for (auto i = start; i <= end; ++i) {
 		_table.insert(createDomElem(i));
@@ -1822,7 +1823,7 @@ InternalSortTable* EnumeratedInternalSortTable::add(int i1, int i2) {
 	if (_table.empty()) {
 		return new IntRangeInternalSortTable(i1, i2);
 	}
-	if (isRange() && first()->type() == DomainElementType::DET_INT && last()->type() == DomainElementType::DET_INT
+	if (isRange()
 			&& ((i1 >= first()->value()._int - 1 && i1 <= last()->value()._int + 1) || (i2 >= first()->value()._int - 1 && i2 <= last()->value()._int + 1))) { // TODO overflow checks
 		auto intst = new IntRangeInternalSortTable(first()->value()._int, last()->value()._int);
 		return intst->add(i1, i2);
@@ -1841,8 +1842,7 @@ InternalSortTable* EnumeratedInternalSortTable::add(const DomainElement* d) {
 		if (_table.empty()) {
 			return new IntRangeInternalSortTable(d->value()._int, d->value()._int);
 		}
-		if (isRange() && first()->type() == DomainElementType::DET_INT && last()->type() == DomainElementType::DET_INT
-				&& d->value()._int >= first()->value()._int - 1 && d->value()._int <= last()->value()._int + 1) { // TODO overflow checks
+		if (isRange() && d->value()._int >= first()->value()._int - 1 && d->value()._int <= last()->value()._int + 1) { // TODO overflow checks
 			auto intst = new IntRangeInternalSortTable(first()->value()._int, last()->value()._int);
 			return intst->add(d);
 		}
@@ -1853,6 +1853,9 @@ InternalSortTable* EnumeratedInternalSortTable::add(const DomainElement* d) {
 		ist->add(d);
 		return ist;
 	} else {
+		if (d->type() != DomainElementType::DET_INT) {
+			nbNotIntElements++;
+		}
 		_table.insert(d);
 		return this;
 	}
@@ -1867,6 +1870,9 @@ InternalSortTable* EnumeratedInternalSortTable::remove(const DomainElement* d) {
 		ist->remove(d);
 		return ist;
 	} else {
+		if(d->type()!=DomainElementType::DET_INT){
+			nbNotIntElements--;
+		}
 		_table.erase(d);
 		return this;
 	}

src/structure/StructureComponents.hpp
@@ -1202,6 +1202,7 @@ class IntRangeInternalSortTable;
 class EnumeratedInternalSortTable: public InternalSortTable {
 private:
 	SortedElementTuple _table;
+	int nbNotIntElements; // Used for efficient range operations
 
 	bool contains(const DomainElement*) const;
 
@@ -1233,16 +1234,19 @@ protected:
 	void addNonRange(int start1, int end1, int start2, int end2);
 
 public:
-	EnumeratedInternalSortTable() {
+	EnumeratedInternalSortTable(): nbNotIntElements(0) {
 	}
 	EnumeratedInternalSortTable(const SortedElementTuple& d) :
-			_table(d) {
+			_table(d), nbNotIntElements(0) {
 	}
 	InternalSortTable* add(const DomainElement*);
 	InternalSortTable* remove(const DomainElement*);
 	InternalSortTable* add(int i1, int i2);
 	const DomainElement* first() const;
 	const DomainElement* last() const;
+	/**
+	 * Returns true if the table is not empty, only contains integers and the integers cover a full interval and nothing outside
+	 */
 	bool isRange() const;
 
 	// Visitor

src/insert.cpp
@@ -2037,28 +2037,23 @@ void Insert::addElement(SortTable* s, char c1, char c2) const {
 }
 
 SortTable* Insert::createSortTable() const {
-	auto eist = new EnumeratedInternalSortTable();
-	return new SortTable(eist);
+	return TableUtils::createSortTable();
 }
 
 void Insert::truepredinter(NSPair* nst) const {
-	auto eipt = new EnumeratedInternalPredTable();
-	auto pt = new PredTable(eipt, Universe(vector<SortTable*>(0)));
+	auto pt = TableUtils::createPredTable(Universe(vector<SortTable*>(0)));
 	ElementTuple et;
 	pt->add(et);
 	predinter(nst, pt);
 }
 
 void Insert::falsepredinter(NSPair* nst) const {
-	auto eipt = new EnumeratedInternalPredTable();
-	auto pt = new PredTable(eipt, Universe(vector<SortTable*>(0)));
+	auto pt = TableUtils::createPredTable(Universe(vector<SortTable*>(0)));
 	predinter(nst, pt);
 }
 
 PredTable* Insert::createPredTable(unsigned int arity) const {
-	auto eipt = new EnumeratedInternalPredTable();
-	auto pt = new PredTable(eipt, TableUtils::fullUniverse(arity));
-	return pt;
+	return TableUtils::createPredTable(TableUtils::fullUniverse(arity));
 }
 
 void Insert::addTuple(PredTable* pt, ElementTuple& tuple, YYLTYPE l) const {

tests/insertiontests.cpp
@@ -12,7 +12,6 @@
 
 #include "gtest/gtest.h"
 #include "GlobalData.hpp"
-#include "insert.hpp"
 #include "structure/MainStructureComponents.hpp"
 #include "commontypes.hpp"
 #include "inferences/grounding/Utils.hpp"
@@ -25,8 +24,7 @@ using namespace std;
 namespace Tests {
 
 TEST(Insertion, EmptyTable) {
-	auto& insert = getGlobal()->getInserter();
-	auto table = insert.createSortTable();
+	auto table = TableUtils::createSortTable();
 	ASSERT_FALSE(table->isRange()); // Empty tables cannot be ranges
 	table->add(createDomElem(1));
 	ASSERT_TRUE(table->isRange());
@@ -44,8 +42,7 @@ TEST(Insertion, EmptyTable) {
 }
 
 TEST(Insertion, InRangeTable) {
-	auto& insert = getGlobal()->getInserter();
-	auto table = insert.createSortTable();
+	auto table = TableUtils::createSortTable();
 	ASSERT_FALSE(table->isRange()); // Empty tables cannot be ranges
 	table->add(1,10);
 	table->add(5,2);
@@ -53,11 +50,19 @@ TEST(Insertion, InRangeTable) {
 }
 
 TEST(Insertion, OutTable) {
-	auto& insert = getGlobal()->getInserter();
-	auto table = insert.createSortTable();
+	auto table = TableUtils::createSortTable();
 	ASSERT_FALSE(table->isRange()); // Empty tables cannot be ranges
 	table->add(-5,5);
 	table->add(-100,-7);
+	ASSERT_FALSE(table->isRange());
+}
+
+TEST(Insertion, WithFloatTable) {
+	auto table = TableUtils::createSortTable();
+	ASSERT_FALSE(table->isRange()); // Empty tables cannot be ranges
+	table->add(-5,5);
+	ASSERT_TRUE(table->isRange());
+	table->add(createDomElem(0.3));
 	ASSERT_FALSE(table->isRange());
 }
 

commit 25a0b1ca521b69fbb1f5c8ce6f3d55245ca1f13a

Fixed issue with ranges
Fixes a bug introduced in 91588a8be3c2.
In that commit a counter for the number of non-int elements was created.
However, in the copy-constructor, this was not initialized correctly.

src/structure/StructureComponents.cpp
@@ -1835,6 +1835,16 @@ InternalSortTable* EnumeratedInternalSortTable::add(int i1, int i2) {
 	return this;
 }
 
+void EnumeratedInternalSortTable::countNBNotIntElements(){
+	nbNotIntElements = 0;
+	for(auto el: _table){
+		if(el->type() != DET_INT){
+			nbNotIntElements++;
+		}
+	}
+}
+
+
 InternalSortTable* EnumeratedInternalSortTable::add(const DomainElement* d) {
 	if (contains(d)) {
 		return this;

src/structure/StructureComponents.hpp
@@ -1224,6 +1224,8 @@ private:
 	tablesize size() const {
 		return tablesize(TST_EXACT, _table.size());
 	}
+	void countNBNotIntElements();
+
 protected:
 	// SortTable is responsible for ref management and deletion!
 	~EnumeratedInternalSortTable() {
@@ -1236,8 +1238,10 @@ protected:
 public:
 	EnumeratedInternalSortTable(): nbNotIntElements(0) {
 	}
-	EnumeratedInternalSortTable(const SortedElementTuple& d) :
-			_table(d), nbNotIntElements(0) {
+	EnumeratedInternalSortTable(const SortedElementTuple& d)
+			: 	_table(d),
+				nbNotIntElements(0) {
+		countNBNotIntElements();
 	}
 	InternalSortTable* add(const DomainElement*);
 	InternalSortTable* remove(const DomainElement*);