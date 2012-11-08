5877b65a3a57d523ec19272c935718184022105a
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-11-08 11:59:02 +0100

    Fixes #263 PATH is extended when running runidp.sh

--- scripts/runidp.sh
+++ scripts/runidp.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 export LD_LIBRARY_PATH=$DIR/../lib/:$LD_LIBRARY_PATH
+export PATH=$PATH:$DIR/../
 export DYLD_LIBRARY_PATH=$DIR/../lib/:$DYLD_LIBRARY_PATH
 exec "$DIR/idp" -d "$DIR/../" "$@"
