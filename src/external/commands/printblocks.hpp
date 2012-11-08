bb73569bd5d8bcf1fdaa368b49e0cb73bf62dc30
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-11-08 11:28:49 +0100

    Refactored print inferences for blocks into one template class.

--- src/external/commands/printblocks.hpp
+++ src/external/commands/printblocks.hpp
@@ -8,14 +8,12 @@
  * Celestijnenlaan 200A, B-3001 Leuven, Belgium
 ****************************************************************/
 
-#ifndef PRINTSTRUCTURE_HPP_
-#define PRINTSTRUCTURE_HPP_
+#pragma once
 
 #include "commandinterface.hpp"
 #include "printers/print.hpp"
 #include "IncludeComponents.hpp"
 #include "theory/Query.hpp"
-#include "structure/MainStructureComponents.hpp"
 
 #include "options.hpp"
 
@@ -30,116 +28,16 @@ std::string print(Object o) {
 	return stream.str();
 }
 
-typedef TypedInference<LIST(AbstractStructure*)> PrintStructureInferenceBase;
-class PrintStructureInference: public PrintStructureInferenceBase {
+template<typename Base>
+class PrintInference: public TypedInference<Base> {
 public:
-	PrintStructureInference()
-			: PrintStructureInferenceBase("tostring", "Prints the given structure.") {
-		setNameSpace(getInternalNamespaceName());
+	PrintInference()
+			: TypedInference<Base>("tostring", "Prints the given object.") {
+		TypedInference<Base>::setNameSpace(getInternalNamespaceName());
 	}
 
 	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto structure = get<0>(args);
-		return InternalArgument(StringPointer(print(structure)));
+		auto a = this-> template get<0>(args);
+		return InternalArgument(StringPointer(print(a)));
 	}
 };
-
-typedef TypedInference<LIST(Namespace*)> PrintNamespaceInferenceBase;
-class PrintNamespaceInference: public PrintNamespaceInferenceBase {
-public:
-	PrintNamespaceInference()
-			: PrintNamespaceInferenceBase("tostring", "Prints the given namespace.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto space = get<0>(args);
-		return InternalArgument(StringPointer(print(space)));
-	}
-};
-
-typedef TypedInference<LIST(AbstractTheory*)> PrintTheoryInferenceBase;
-class PrintTheoryInference: public PrintTheoryInferenceBase {
-public:
-	PrintTheoryInference()
-			: PrintTheoryInferenceBase("tostring", "Prints the given theory.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto theory = get<0>(args);
-		return InternalArgument(StringPointer(print(theory)));
-	}
-};
-
-typedef TypedInference<LIST(Query*)> PrintQueryInferenceBase;
-class PrintQueryInference: public PrintQueryInferenceBase {
-public:
-	PrintQueryInference()
-			: PrintQueryInferenceBase("tostring", "Prints the given query.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto query = get<0>(args);
-		return InternalArgument(StringPointer(print(query)));
-	}
-};
-
-typedef TypedInference<LIST(Term*)> PrintTermInferenceBase;
-class PrintTermInference: public PrintTermInferenceBase {
-public:
-	PrintTermInference()
-			: PrintTermInferenceBase("tostring", "Prints the given term.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto term = get<0>(args);
-		return InternalArgument(StringPointer(print(term)));
-	}
-};
-
-typedef TypedInference<LIST(Formula*)> PrintFormulaInferenceBase;
-class PrintFormulaInference: public PrintFormulaInferenceBase {
-public:
-	PrintFormulaInference()
-			: PrintFormulaInferenceBase("tostring", "Prints the given formula.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto f = get<0>(args);
-		return InternalArgument(StringPointer(print(f)));
-	}
-};
-
-typedef TypedInference<LIST(Vocabulary*)> PrintVocabularyInferenceBase;
-class PrintVocabularyInference: public PrintVocabularyInferenceBase {
-public:
-	PrintVocabularyInference()
-			: PrintVocabularyInferenceBase("tostring", "Prints the given vocabulary.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto vocabulary = get<0>(args);
-		return InternalArgument(StringPointer(print(vocabulary)));
-	}
-};
-
-typedef TypedInference<LIST(const PredTable*)> PrintPredTableInferenceBase;
-class PrintPredTableInference: public PrintPredTableInferenceBase {
-public:
-	PrintPredTableInference()
-			: PrintPredTableInferenceBase("tostring", "Prints the given predicate table.") {
-		setNameSpace(getInternalNamespaceName());
-	}
-
-	InternalArgument execute(const std::vector<InternalArgument>& args) const {
-		auto table = get<0>(args);
-		return InternalArgument(StringPointer(toString(table)));
-	}
-};
-
-#endif /* PRINTSTRUCTURE_HPP_ */
