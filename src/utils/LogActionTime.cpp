1408b9ae65ac63bc0be433ced0d21e572b7fb46f
Broes De Cat <broes.decat@cs.kuleuven.be>
2012-12-05 18:22:43 +0100

    Moved logaction to separate files.

---
+++ src/utils/LogActionTime.cpp
@@ -0,0 +1,15 @@
+/*
+ * LogActionTime.cpp
+ *
+ *  Created on: Dec 3, 2012
+ *      Author: broes
+ */
+
+#include "LogActionTime.hpp"
+#include <cstdio>
+#include <iostream>
+using namespace std;
+
+void logActionAndTime(const std::string& action){
+	std::clog <<action <<" (" <<(long long)((double)clock()*1000/(CLOCKS_PER_SEC)) <<")\n";
+}
---
+++ src/utils/LogActionTime.hpp
@@ -0,0 +1,12 @@
+/*
+ * LogActionTime.hpp
+ *
+ *  Created on: Dec 3, 2012
+ *      Author: broes
+ */
+
+#pragma once
+
+#include <string>
+
+void logActionAndTime(const std::string& action);
