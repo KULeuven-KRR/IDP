KULeuven-KRR/IDP

commit f8ec2ecfc9c2eea2024ba044807bd5361bea9c4d

Bugfix in Fobdds
Changed the ordering of kernels such that the If-Then-Else operation
needs to swap kernels less.

src/fobdds/FoBddUtils.hpp
@@ -29,7 +29,11 @@ using namespace rel_ops;
 /**
  *	A kernel order contains two numbers to order kernels (nodes) in a BDD.
  *	Kernels with a higher category appear further from the root than kernels with a lower category
- *	Within a category, kernels are ordered according to the second number.
+ *	Within a category, kernels are ordered reversed according to the second number.
+ *
+ *	The reason for this reversed ordering is to avoid too many reorderings
+ *	(during several operations (quantify) we create much new
+ *	bdds and don't want every of them to be moved to the top of the BDD
  */
 struct KernelOrder {
 	KernelOrderCategory _category; //!< The category of this kernel
@@ -46,7 +50,7 @@ struct KernelOrder {
 		} else if (ko._category < _category) {
 			return false;
 		} else {
-			return _number < ko._number;
+			return _number > ko._number;
 		}
 	}
 };

commit 82e7c97b147fc555408566340e9174602b3bdfcc

Bugfix in BDDManager:
Commit f8ec2ec introduced a bug: it changed the ordering of kernels but
some other code was not adapted accordingly.
This caused a bug in optimizequery because moving up didn't work well
-> Not all kernels were moved up!
This is now fixed and, in general, the kernelAbove and kernelBelow methods
can be used now. Also added Asserts in these methods to avoid similar
future mistakes

src/fobdds/FoBddManager.cpp
@@ -75,19 +75,46 @@ void FOBDDManager::clearDynamicTables() {
 	_quanttable.clear();
 }
 
+FOBDDKernel* FOBDDManager::kernelAbove(const FOBDDKernel* kernel) {
+	auto cat = kernel->category();
+	if (cat == KernelOrderCategory::TRUEFALSECATEGORY) {
+		return NULL;
+	}
+	Assert(_kernels.find(cat) != _kernels.cend());
+	auto catkernels = _kernels[cat];
+
+	unsigned int nr = kernel->number();
+	auto kernelabove = catkernels.find(nr + 1);
+	if (kernelabove != catkernels.cend()) {
+		Assert(*(kernelabove->second) < *kernel);
+		return kernelabove->second;
+	}
+	return NULL;
+
+}
+FOBDDKernel* FOBDDManager::kernelBelow(const FOBDDKernel* kernel) {
+	auto cat = kernel->category();
+	if (cat == KernelOrderCategory::TRUEFALSECATEGORY) {
+		return NULL;
+	}
+	Assert(_kernels.find(cat) != _kernels.cend());
+	auto catkernels = _kernels[cat];
+	unsigned int nr = kernel->number();
+	auto kernelbelow = catkernels.find(nr - 1);
+	if (kernelbelow != catkernels.cend()) {
+		Assert((*kernelbelow->second) > *kernel);
+		return kernelbelow->second;
+	}
+	return NULL;
+}
+
 void FOBDDManager::moveUp(const FOBDDKernel* kernel) {
 	clearDynamicTables();
-	auto cat = kernel->category();
-	if (cat != KernelOrderCategory::TRUEFALSECATEGORY) {
-		unsigned int nr = kernel->number();
-		if (nr != 0) {
-			--nr;
-			Assert(_kernels.find(cat) != _kernels.cend());
-			Assert(_kernels[cat].find(nr) != _kernels[cat].cend());
-			const FOBDDKernel* pkernel = _kernels[cat][nr];
-			moveDown(pkernel);
-		}
+	auto kernelabove = kernelAbove(kernel);
+	if(kernelabove == NULL){
+		return;
 	}
+	moveDown(kernelabove);
 }
 
 void FOBDDManager::moveDown(const FOBDDKernel* kernel) {
@@ -97,11 +124,11 @@ void FOBDDManager::moveDown(const FOBDDKernel* kernel) {
 		unsigned int nr = kernel->number();
 		vector<const FOBDD*> falseerase;
 		vector<const FOBDD*> trueerase;
-		Assert(_kernels.find(cat) != _kernels.cend());
-		if (_kernels[cat].find(nr + 1) == _kernels[cat].cend()) {
+		const FOBDDKernel* nextkernel = kernelBelow(kernel);
+		if(nextkernel == NULL){
 			return;
 		}
-		const FOBDDKernel* nextkernel = _kernels[cat][nr + 1];
+		auto nextKernelNumber = nextkernel->number();
 		//bdds contains all bdds with kernel as kernel
 		const MBDDMBDDBDD& bdds = _bddtable[kernel];
 		for (auto it = bdds.cbegin(); it != bdds.cend(); ++it) {
@@ -151,11 +178,11 @@ void FOBDDManager::moveDown(const FOBDDKernel* kernel) {
 		}
 		//We make new kernels, because previously the originals are const.
 		FOBDDKernel* tkernel = _kernels[cat][nr];
-		FOBDDKernel* nkernel = _kernels[cat][nr + 1];
+		FOBDDKernel* nkernel = _kernels[cat][nextKernelNumber];
 		nkernel->replacenumber(nr);
-		tkernel->replacenumber(nr + 1);
+		tkernel->replacenumber(nextKernelNumber);
 		_kernels[cat][nr] = nkernel;
-		_kernels[cat][nr + 1] = tkernel;
+		_kernels[cat][nextKernelNumber] = tkernel;
 	}
 }
 
@@ -1449,15 +1476,14 @@ void FOBDDManager::optimizeQuery(const FOBDD* query, const fobddvarset& vars, co
 	for (auto it = kernels.cbegin(); it != kernels.cend(); ++it) {
 		CHECKTERMINATION;
 		// move kernel to the top
-		while ((*it)->number() != 0) {
+		while (kernelAbove(*it) != NULL) {
 			moveUp(*it);
 		}
-
 		double bestscore = BddStatistics::estimateCostAll(query, vars, indices, structure, this);
 		int bestposition = 0;
 		//AT THIS POINT: bestposition is the number of "movedowns" needed from the top to get to bestpositions
 		// move downward
-		while ((*it)->number() < _kernels[(*it)->category()].size() - 1) {
+		while (kernelBelow(*it) != NULL) {
 			moveDown(*it);
 			double currscore = BddStatistics::estimateCostAll(query, vars, indices, structure, this);
 			if (currscore < bestscore) {

src/fobdds/FoBddManager.hpp
@@ -272,6 +272,9 @@ private:
 
 	void moveDown(const FOBDDKernel*); //!< Swap the given kernel with its successor in the kernelorder
 	void moveUp(const FOBDDKernel*); //!< Swap the given kernel with its predecessor in the kernelorder
+
+	FOBDDKernel* kernelAbove(const FOBDDKernel*); //!< Returns the kernel of the same category, directly above the given one. Returns NULL if none such exists
+	FOBDDKernel* kernelBelow(const FOBDDKernel*); //!< Returns the kernel of the same category, directly below the given one. Returns NULL if none such exists
 };
 
 fobddvarset variables(const FOBDDKernel*, FOBDDManager* manager);

src/fobdds/FoBddUtils.hpp
@@ -37,6 +37,8 @@ using namespace rel_ops;
  *	The reason for this reversed ordering is to avoid too many reorderings
  *	(during several operations (quantify) we create much new
  *	bdds and don't want every of them to be moved to the top of the BDD
+ *
+ *	kernelAbove and kernelBelow are based on this order!!!
  */
 struct KernelOrder {
 	KernelOrderCategory _category; //!< The category of this kernel