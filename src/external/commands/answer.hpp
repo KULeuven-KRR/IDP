c3669d35d6356aa5cd908bf8c3f5937468d81fc7
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2013-04-19 15:53:55 +0200

    added support for question answering

---
+++ src/external/commands/answer.hpp
@@ -0,0 +1,27 @@
+/*****************************************************************************
+ * Copyright 2010-2012 Katholieke Universiteit Leuven
+ *
+ * Use of this software is governed by the GNU LGPLv3.0 license
+ *
+ * Written by Broes De Cat, Bart Bogaerts, Stef De Pooter, Johan Wittocx,
+ * Jo Devriendt, Joachim Jansen and Pieter Van Hertum 
+ * K.U.Leuven, Departement Computerwetenschappen,
+ * Celestijnenlaan 200A, B-3001 Leuven, Belgium
+ ****************************************************************************/
+
+#pragma once
+
+#include "commandinterface.hpp"
+#include "IncludeComponents.hpp"
+
+class AnswerInference: public StringBase {
+public:
+	AnswerInference()
+			: StringBase("answer", "Answers any question.") {
+		setNameSpace(getInternalNamespaceName());
+	}
+
+	InternalArgument execute(const std::vector<InternalArgument>& args) const {
+		return InternalArgument(42);
+	}
+};
