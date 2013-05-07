eaaf7fa0533486097f0438492ac557aa38ac6b7f
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2013-05-07 14:36:42 +0200

    Added new method to split an LTC vocabulary
    LTC vocabularies can now be split into a state voc and a bistate voc

---
+++ src/inferences/progression/transformLTCVocabulary.hpp
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Copyright 2010-2012 Katholieke Universiteit Leuven
+ *
+ * Use of this software is governed by the GNU LGPLv3.0 license
+ *
+ * Written by Broes De Cat, Bart Bogaerts, Stef De Pooter, Johan Wittocx,
+ * Jo Devriendt, Joachim Jansen and Pieter Van Hertum 
+ * K.U.Leuven, Departement Computerwetenschappen,
+ * Celestijnenlaan 200A, B-3001 Leuven, Belgium
+ ****************************************************************************/
+
+#pragma once
+
+#include <vector>
+#include <map>
+#include "data/StateVocInfo.hpp"
+#include "data/LTCData.hpp"
+
+class Vocabulary;
+class Sort;
+class PFSymbol;
+class Predicate;
+class Function;
+class LTCInputData;
+
+
+class LTCVocabularyTransformer {
+	const Vocabulary* _ltcVoc;
+	LTCInputData _inputData;
+
+	LTCVocInfo* _result;
+
+public:
+	static LTCVocInfo* TransformVocabulary(const Vocabulary* ltcVoc, LTCInputData inputData){
+		auto g = LTCVocabularyTransformer(ltcVoc, inputData);
+		return g.transform();
+	}
+private:
+
+	LTCVocabularyTransformer(const Vocabulary* ltcVoc, LTCInputData inputData);
+	~LTCVocabularyTransformer();
+
+	LTCVocInfo* transform();
+
+	std::vector<PFSymbol*> projectSymbol(PFSymbol* p);
+	std::vector<Sort*> projectSorts(PFSymbol* p);
+	void projectAndAddSymbol(PFSymbol* pred);
+};
+
+
+
