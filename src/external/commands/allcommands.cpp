65522d3e0504f4b706ae5958ac78a0d9e5a74a89
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2013-12-10 12:27:35 +0100

    Made invariant checking public

--- src/external/commands/allcommands.cpp
+++ src/external/commands/allcommands.cpp
@@ -133,6 +133,7 @@ const vector<shared_ptr<Inference>>& getAllInferences() {
 	inferences.push_back(make_shared<ProgressInference>());
 	inferences.push_back(make_shared<InitInference>());
 	inferences.push_back(make_shared<InitInferenceNoTime>());
+	inferences.push_back(make_shared<InvariantInference>());
 
 	inferences.push_back(make_shared<AnswerInference>());
 	return inferences;
