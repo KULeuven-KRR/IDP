4b14e16f19655f2ffde7c92a6f8d939643308949
Broes De Cat <broes.decat@cs.kuleuven.be>
2014-01-03 11:05:35 +0100

    Function detection tests.

--- tests/EntailmentTests.cpp
+++ tests/EntailmentTests.cpp
@@ -13,14 +13,17 @@
 
 #include "gtest/gtest.h"
 #include "external/runidp.hpp"
+#include "creation/cppinterface.hpp"
 #include "options.hpp"
 #include "GlobalData.hpp"
 #include "utils/FileManagement.hpp"
 #include "TestUtils.hpp"
+#include "inferences/functiondetection/FunctionDetection.hpp"
 
 #include <exception>
 
 using namespace std;
+using namespace Gen;
 
 namespace Tests {
 
@@ -40,5 +43,69 @@ TEST_P(EntailmentTests, Entails) {
 	runTests("entailment.idp", GetParam(), "checkEntails()");
 }
 INSTANTIATE_TEST_CASE_P(Entailment, EntailmentTests, ::testing::ValuesIn(generateListOfEntailmentFiles()));
+
+TEST(FunctionDetection, Skolemize){
+	Vocabulary V("FuncDetect");
+	auto s = new Sort("X");
+	V.add(s);
+	auto p = Gen::pred("P", {s,s});
+	V.add(p.p());
+	Theory T("T", &V,{});
+	auto var = new Variable(s);
+	auto var2 = new Variable(s);
+	T.add(&Gen::forall(var,Gen::exists(var2, p({var,var2}))));
+	FunctionDetection::doDetectAndRewriteIntoFunctions(&T, true);
+	ASSERT_EQ(V.getFuncs().size()+1, T.vocabulary()->getFuncs().size());  // TODO and should be partial! TODO for easier checking, return detected function dependencies?
+}
+
+TEST(FunctionDetection, Pred2PartialFunc){
+	Vocabulary V("FuncDetect");
+	auto s = new Sort("X");
+	V.add(s);
+	auto p = Gen::pred("P", {s,s});
+	V.add(p.p());
+	Theory T("T", &V,{});
+	auto var = new Variable(s);
+	auto var2 = new Variable(s);
+	auto var3 = new Variable(s);
+	T.add(&Gen::forall({var,var2,var3},Gen::disj({p({var,var2}).negate(),p({var,var3}).negate(),&PredWrapper(get(STDPRED::EQ, s))({var2,var3})})));
+	FunctionDetection::doDetectAndRewriteIntoFunctions(&T, true);
+	ASSERT_EQ(V.getFuncs().size()+1, T.vocabulary()->getFuncs().size());  // TODO and should be partial! TODO for easier checking, return detected function dependencies?
+}
+
+TEST(FunctionDetection, Pred2TotalFunc){
+	Vocabulary V("FuncDetect");
+	auto s = new Sort("X");
+	V.add(s);
+	auto p = Gen::pred("P", {s,s});
+	V.add(p.p());
+	Theory T("T", &V,{});
+	auto var = new Variable(s);
+	auto var2 = new Variable(s);
+	auto var3 = new Variable(s);
+	T.add(&Gen::forall(var,Gen::exists(var2, p({var, var2}))));
+	T.add(&Gen::forall({var,var2,var3},Gen::disj({p({var,var2}).negate(),p({var,var3}).negate(),&PredWrapper(get(STDPRED::EQ, s))({var2,var3})})));
+	FunctionDetection::doDetectAndRewriteIntoFunctions(&T, true);
+	ASSERT_EQ(V.getFuncs().size()+1, T.vocabulary()->getFuncs().size());
+}
+
+TEST(FunctionDetection, Pred2TotalDefFunc){
+	Vocabulary V("FuncDetect");
+	auto s = new Sort("X");
+	V.add(s);
+	auto p = Gen::pred("P", {s,s});
+	auto q = Gen::pred("Q", {s,s});
+	V.add(p.p());
+	V.add(q.p());
+	Theory T("T", &V,{});
+	auto var = new Variable(s);
+	auto var2 = new Variable(s);
+	auto var3 = new Variable(s);
+	T.add(&Gen::forall(var,Gen::exists(var2, p({var, var2}))));
+	T.add(&Gen::forall({var,var2,var3},Gen::disj({p({var,var2}).negate(),p({var,var3}).negate(),&PredWrapper(get(STDPRED::EQ, s))({var2,var3})})));
+	T.add((new Definition())->add(new Rule(getVarSet(std::vector<Variable*>{var,var2}),&q({var,var2}), &p({var,var2}),{})));
+	FunctionDetection::doDetectAndRewriteIntoFunctions(&T, true);
+	ASSERT_EQ(V.getFuncs().size()+2, T.vocabulary()->getFuncs().size());
+}
 
 }
