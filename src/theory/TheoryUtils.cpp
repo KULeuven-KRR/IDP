4054a8f3a1160b1ecec4cd210cc89b18a34d50be
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2014-02-10 14:38:14 +0100

    added extra const to declaration

--- src/theory/TheoryUtils.cpp
+++ src/theory/TheoryUtils.cpp
@@ -755,14 +755,14 @@ std::map<Variable*, QuantType> collectQuantifiedVariables(AbstractTheory* f, boo
 	return t.execute(f, recursive);
 }
 
-std::set<PFSymbol*> collectSymbols(Formula* f) {
+std::set<PFSymbol*> collectSymbols(const Formula* f) {
 	return transform<CollectSymbols, std::set<PFSymbol*> >(f);
 }
-std::set<PFSymbol*> collectSymbols(Rule* f) {
+std::set<PFSymbol*> collectSymbols(const Rule* f) {
 	return transform<CollectSymbols, std::set<PFSymbol*> >(f);
 
 }
-std::set<PFSymbol*> collectSymbols(AbstractTheory* f) {
+std::set<PFSymbol*> collectSymbols(const AbstractTheory* f) {
 	return transform<CollectSymbols, std::set<PFSymbol*> >(f);
 
 }
