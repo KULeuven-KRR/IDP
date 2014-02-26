e1cc974fe5c53954cc4c9408c701ebd9d35d3bcf
Bart Bogaerts <bart.bogaerts@cs.kuleuven.be>
2014-02-26 16:53:44 +0100

    removed warning

--- src/structure/StructureComponents.cpp
+++ src/structure/StructureComponents.cpp
@@ -2000,7 +2000,7 @@ bool ConstructedInternalSortTable::empty() const {
 	if(isRecursive()){
 		for(auto f:_constructors){
 			auto allnonrec = true;
-			for(auto i=0; i<f->arity(); ++i){
+			for(u_int i=0; i<f->arity(); ++i){
 				if(f->insorts()[i]==f->outsort()){
 					allnonrec = false;
 					break;
@@ -2034,7 +2034,7 @@ bool ConstructedInternalSortTable::approxEmpty() const {
 	if(isRecursive()){
 		for(auto f:_constructors){
 			auto allnonrec = true;
-			for(auto i=0; i<f->arity(); ++i){
+			for(u_int i=0; i<f->arity(); ++i){
 				if(f->insorts()[i]==f->outsort()){
 					allnonrec = false;
 					break;
