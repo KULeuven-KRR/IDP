8b02c0a5f63aa0b5479aec0853dccefcbba55ef5
Broes De Cat <broes.decat@cs.kuleuven.be>
2014-03-19 14:58:22 +0100

    Added unit testing of function detection.

---
+++ tests/mxtests/funcdetect.cpp
@@ -0,0 +1,20 @@
+/*****************************************************************************
+ * Copyright 2010-2012 Katholieke Universiteit Leuven
+ *
+ * Use of this software is governed by the GNU LGPLv3.0 license
+ *
+ * Written by Broes De Cat, Bart Bogaerts, Stef De Pooter, Johan Wittocx,
+ * Jo Devriendt, Joachim Jansen and Pieter Van Hertum 
+ * K.U.Leuven, Departement Computerwetenschappen,
+ * Celestijnenlaan 200A, B-3001 Leuven, Belgium
+ ****************************************************************************/
+
+#include "FileEnumerator.hpp"
+
+namespace Tests {
+
+TEST_P(SimpleMXnbTest, DoesMXWithFunctionDetection) {
+	runTests("modelexpansion.idp", GetParam(), "mxfuncdetect()");
+}
+
+}
