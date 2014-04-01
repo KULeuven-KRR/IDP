b501e4844f5d8272600ba02d810c4d140591c42e
Joachim Jansen <joachim.jansen@cs.kuleuven.be>
2014-04-01 17:38:15 +0200

    Removed skipping of non-total definition in definition refinement

--- src/inferences/definitionevaluation/refineStructureWithDefinitions.cpp
+++ src/inferences/definitionevaluation/refineStructureWithDefinitions.cpp
@@ -118,9 +118,6 @@ DefinitionRefiningResult refineStructureWithDefinitions::refineDefinedSymbols(Th
 	}
 	theory = FormulaUtils::improveTheoryForInference(theory, structure, false, false);
 	auto opens = DefinitionUtils::opens(theory->definitions()); // Collect the open symbols of all definitions
-	if (getOption(BoolType::STABLESEMANTICS)) {
-		CalculateDefinitions::removeNonTotalDefnitions(opens);
-	}
 	Structure* initialStructure = structure->clone(); // Used at the end to determine consistency
 	FormulaUtils::removeInterpretationOfDefinedSymbols(theory,structure);
 	DefinitionRefiningResult result(structure);
