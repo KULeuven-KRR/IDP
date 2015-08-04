b6142a42a8e4cc6be78ee0a2f97f0c75e742df29
JoD <jo.devriendt@student.kuleuven.be>
2015-08-04 22:49:38 +0200

    renamed tests to something meaningful - Bart

--- tests/mxtests/mxIterator.cpp
+++ tests/mxtests/mxIterator.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 namespace Tests {
 
-TEST_P(MXnbTest, VerifyAllStructures) {
+TEST_P(MXIteratorTest, FindAllModels) {
 	runTests("modeliteration.idp", GetParam(), "run()");
 }
 
