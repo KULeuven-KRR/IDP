ba2508a775b6d55b56955f9c8fb53d49b90bb205
JoD <jo.devriendt@cs.kuleuven.be>
2016-01-27 18:35:08 +0100

    cleaned up (old) symmetry breaking code

--- src/inferences/symmetrybreaking/Symmetry.hpp
+++ src/inferences/symmetrybreaking/Symmetry.hpp
@@ -17,7 +17,6 @@
 #include <unordered_set>
 #include <vector>
 #include <string>
-#include <list>
 #include <ostream>
 
 #include "visitors/TheoryVisitor.hpp"
@@ -34,136 +33,6 @@ class Variable;
 class OccurrencesCounter;
 class PredInter;
 class FuncInter;
-
-/** 
- *	\brief	Abstract base class to represent a symmetry group
- */
-
-/**
- * Definition of an IVSet: 
- * 	a set of domain elements elements_, 
- * 		occurring in the domain of all the sorts of sorts_,
- * 		not occurring in the domain of all sorts not in sorts_, but whose parents are in sorts_.
- *	a set of sorts sorts_,
- *		which is a fixpoint under the "add parent sorts" operation.
- * 	and a set of PFSymbols relations_, 
- * 		which contains all the relations occurring in a theory who have as argument at least one of the elements of sorts_
- *
- * INVARIANT: every IVSet has getSize()>1.
- * INVARIANT: the domain elements of an IVSet all belong to the youngest sort in sorts_. The other sorts in sorts_ are parents of this youngest sort
- * (unfortunately, the last invariant is not enforced by an assertion, even though it is assumed it holds)
- */
-class IVSet {
-private:
-	// Attributes
-	const Structure* structure_; //!< The structure over which this symmetry ranges
-	const std::set<const DomainElement*> elements_; //!< Elements which are permuted by the symmetry's
-	const std::set<Sort*> sorts_; //!< Sorts considered for the elements 
-	const std::set<PFSymbol*> relations_; //!< Relations which are permuted by the symmetry's
-
-	// Inspectors
-	std::pair<std::list<int>, std::list<int> > getSymmetricLiterals(AbstractGroundTheory*, const DomainElement*, const DomainElement*) const;
-
-public:
-	IVSet(const Structure*, const std::set<const DomainElement*>, const std::set<Sort*>, const std::set<PFSymbol*>);
-
-	~IVSet() {
-	}
-
-	// Mutators
-	// none should come here, class is immutable :)
-
-	// Inspectors
-	const Structure* getStructure() const;
-	const std::set<const DomainElement*>& getElements() const;
-	const std::set<Sort*>& getSorts() const;
-	const std::set<PFSymbol*>& getRelations() const;
-
-	int getSize() const {
-		return getElements().size();
-	}
-	bool containsMultipleElements() const;
-	bool hasRelevantRelationsAndSorts() const;
-	bool isRelevantSymmetry() const;
-	bool isDontCare() const;
-	bool isEnkelvoudig() const;
-	std::vector<const IVSet*> splitBasedOnOccurrences(OccurrencesCounter*) const;
-	std::vector<const IVSet*> splitBasedOnBinarySymmetries() const;
-
-	void addSymBreakingPreds(AbstractGroundTheory*, bool) const;
-	std::vector<std::map<int, int> > getBreakingSymmetries(AbstractGroundTheory*) const;
-	std::vector<std::list<int> > getInterchangeableLiterals(AbstractGroundTheory*) const;
-
-	// Output
-	std::ostream& put(std::ostream& output) const;
-};
-
-std::vector<const IVSet*> findIVSets(const AbstractTheory*, const Structure*, const Term*);
-
-void addSymBreakingPredicates(AbstractGroundTheory*, std::vector<const IVSet*>, bool nbModelsEquivalent);
-
-/**
- * 	Theory analyzing visitor which extracts information relevant for symmetry detection.
- *	More specifically, it will extract
- *		a set of domain elements which should not be permuted by a symmetry,
- *		a set of sorts whose domain elements should not be permuted by a symmetry, and
- *		a set of relations which are used in the theory (to exclude unused but nonetheless defined relations)
- */
-class TheorySymmetryAnalyzer : public DefaultTraversingTheoryVisitor {
-	VISITORFRIENDS()
-private:
-	const Structure* structure_;
-	std::set<Sort*> forbiddenSorts_;
-	std::set<const DomainElement*> forbiddenElements_;
-	std::set<PFSymbol*> usedRelations_;
-
-	void markAsUnfitForSymmetry(const std::vector<Term*>&);
-	void markAsUnfitForSymmetry(Sort*);
-	void markAsUnfitForSymmetry(const DomainElement*);
-
-	const Structure* getStructure() const {
-		return structure_;
-	}
-
-public:
-
-	TheorySymmetryAnalyzer(const Structure* s)
-	: structure_(s) {
-		/*		markAsUnfitForSymmetry(VocabularyUtils::intsort());
-				markAsUnfitForSymmetry(VocabularyUtils::floatsort());
-				markAsUnfitForSymmetry(VocabularyUtils::natsort());*/
-	}
-
-	void analyze(const AbstractTheory* t);
-	void analyzeForOptimization(const Term* t);
-
-	const std::set<Sort*>& getForbiddenSorts() const {
-		return forbiddenSorts_;
-	}
-
-	const std::set<const DomainElement*>& getForbiddenElements() const {
-		return forbiddenElements_;
-	}
-
-	const std::set<PFSymbol*>& getUsedRelations() const {
-		return usedRelations_;
-	}
-
-	void addForbiddenSort(Sort* sort) {
-		forbiddenSorts_.insert(sort);
-	}
-
-	void addUsedRelation(PFSymbol* relation) {
-		usedRelations_.insert(relation);
-	}
-protected:
-	void visit(const PredForm*);
-	void visit(const FuncTerm*);
-	void visit(const DomainTerm*);
-	void visit(const EqChainForm*);
-	void visit(const AggForm*);
-	void visit(const AggTerm*);
-};
 
 class InterchangeabilitySet;
 
@@ -421,8 +290,8 @@ struct SymbArg{
 
 void detectInterchangeability(std::vector<InterchangeabilityGroup*>& out_groups, const AbstractTheory* t, const Structure* s, const Term* obj = nullptr);
 // NOTE: t will be modified, so make sure a clone was made before!
-void getIntchGroups2(AbstractTheory* theo, const Structure* s, std::vector<InterchangeabilityGroup*>& out_groups, std::vector<std::pair<PFSymbol*, unsigned int> >& symbargs); 
+void getIntchGroups(AbstractTheory* theo, const Structure* s, std::vector<InterchangeabilityGroup*>& out_groups, std::vector<std::pair<PFSymbol*, unsigned int> >& symbargs); 
 void getIntchGroups(AbstractTheory* theo, const Structure* s, std::vector<InterchangeabilityGroup*>& out_groups);
-// TODO: fix the "3 and "2"
+
 void detectNeighborhoods(const Theory* t, const Structure* s, const Term* obj, std::vector<const Definition*>& objDefs, 
 		std::vector<InterchangeabilityGroup*>& outHard, std::vector<InterchangeabilityGroup*>& outRelaxed, Theory* relaxedConstraints);
